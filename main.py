23d8f881516babdb1a587db6f3d297b95b069d18
aspdataupskilling <udemyasp@gmail.com>
2024-12-02 11:48:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print('Hello world')
+from sys import exit
+
+import traceback
+
+
+def view_expenses():
+  # Implement view_expenses here
+  pass
+
+
+def main():
+  try:
+    # Implement main page here
+    exit()
+  except Exception:
+    print('Program error')
+    traceback.print_exc()
+
+
+if __name__ == "__main__":
+  main()
