39cf56b716b3c59776f6baea2ba7aa59ee5ee4a0
aspdataupskilling <udemyasp@gmail.com>
2024-12-02 12:30:23 +0000

    Add expense_tracker_v1

---
+++ expense_tracker_v1.py
@@ -0,0 +1,17 @@
+from sys import exit
+
+import traceback
+
+
+def view_expenses():
+  # Implement view_expenses here
+  pass
+
+
+def main_page():
+  try:
+    # Implement main page here
+    exit()
+  except Exception:
+    print('Program error')
+    traceback.print_exc()
