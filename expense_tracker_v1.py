1463ef20ec680cea1cf090db9672fadfbec01a7c
FreedzIII <dellosafreedom@yahoo.com>
2025-01-03 13:22:44 +0000

    Exercise 2

--- expense_tracker_v1.py
+++ expense_tracker_v1.py
@@ -4,16 +4,16 @@ import traceback
 import pandas as pd
 
 # Function to filter the DataFrame
-def filter_expenses(df=None, category=None, month=None, min_amount=None, max_amount=None):
+def filter_expenses(df=None, category=None, month=None, year=None, min_amount=None, max_amount=None):
     filtered_df = df
 
     # Filter by category
     if category:
         filtered_df = filtered_df[filtered_df['Category'] == category]
 
-    # Filter by month
-    if month:
-        filtered_df = filtered_df[filtered_df['Date'].dt.month == month]
+    # Filter by month and year
+    if month and year:
+        filtered_df = filtered_df[(filtered_df['Date'].dt.month == month) & (filtered_df['Date'].dt.year == year)]
 
     # Filter by amount range
     if min_amount is not None:
@@ -23,7 +23,7 @@ def filter_expenses(df=None, category=None, month=None, min_amount=None, max_amo
 
     return filtered_df
 
-def view_expenses():
+def view_expenses(extract = None):
   # Implement view_expenses here
     try:
         file_path = 'expenses.csv'
@@ -34,24 +34,64 @@ def view_expenses():
         exit()
 
     print('-------------------------')
-    print('View Expenses')
-    print('-------------------------')
-    print('1) View all expenses')
-    print('2) Filtered by month')
+    if extract is False:
+        print('View Expenses')
+        print('-------------------------')
+        print('1) View all expenses')
+    else:
+        print('Extract Expenses')
+        print('-------------------------')
+        print('1) Extract all expenses')
+    print('2) Filtered by date')
     print('3) Filtered by category')
     print('4) Filtered by amount')
     print('x) Go back')
     print('-------------------------')
     option = input('Choose an option > ')
     if option == '1':
-        print(df)
-    elif option == '2':
-        op2In = input("Enter the month to filter by (1-12). Enter 'x' to go back > ")
-        if op2In == 'x':
-            view_expenses()
+        if extract is False:
+            print(df)
         else:
-            filtered_data = filter_expenses(df=df,month=int(op2In))
-            print(filtered_data)
+            save_to_csv(df,'for all expenses')
+
+    elif option == '2':
+        while True:
+            op2Month = input("Enter the month to filter by (1-12). Enter 'x' to go back > ")
+            op2Year = input("Enter the year to filter from year 1970 onwards. Enter 'x' to go back > ")
+
+            if op2Month == 'x' or  op2Year == 'x':
+                view_expenses(extract)
+                break
+
+            try:
+                op2Month = int(op2Month)
+                if op2Month < 1 or op2Month > 12:
+                    print('Invalid month: ', op2Month)
+                    print('Try again')
+                    continue
+            except ValueError:
+                print('Invalid month: ', op2Month)
+                print('Try again')
+                continue
+
+            try:
+                op2Year = int(op2Year)
+                if op2Year < 1970:
+                    print('Invalid year: ', op2Year)
+                    print('Try again')
+                    continue
+            except:
+                print('Invalid year: ', op2Year)
+                print('Try again')
+                continue
+
+            filtered_data = filter_expenses(df=df,month=op2Month,year=op2Year)
+            if extract is False:
+                print(filtered_data)
+            else:
+                save_to_csv(filtered_data,'based on the filter by date')
+            break
+
     elif option == '3':
         print('-------------------------')
         print('Filter by Category')
@@ -61,27 +101,74 @@ def view_expenses():
         print('3) Transpo')
         print('4) Others')
         print('x) Exit')
-        op3In = input('Choose the category to filter by > ')
-        if op3In == 'x':
-            view_expenses()
-        else:
-            if op3In == '1': op3In = "Food"
-            elif op3In == '2': op3In = "Utilities"
-            elif op3In == '3': op3In = "Transpo"
-            elif op3In == '4': op3In = "Others"
-            filtered_data = filter_expenses(df=df,category=op3In)
-            print(filtered_data)
+        print('-------------------------')
+        while True:
+            op3In = input('Choose the category to filter by > ')
+            if op3In == 'x':
+                view_expenses(extract)
+                break
+            else:
+                if op3In == '1': op3In = "Food"
+                elif op3In == '2': op3In = "Utilities"
+                elif op3In == '3': op3In = "Transpo"
+                elif op3In == '4': op3In = "Others"
+                else:
+                    print('Invalid Category')
+                    continue
+                filtered_data = filter_expenses(df=df,category=op3In)
+                if extract is False:
+                    print(filtered_data)
+                else:
+                    save_to_csv(filtered_data,'based on the filter by category')
+                break
+
     elif option == '4':
-        op4InMin = input("Enter minimum amount (Enter 'x' to exit > ")
-        op4InMax = input("Enter maximum amount (Enter 'x' to exit > ")
-        if op4InMin == 'x' or op4InMax == 'x':
-            view_expenses()
-        else:
-            filtered_data = filter_expenses(df=df, min_amount=int(op4InMin), max_amount=int(op4InMax))
-            print(filtered_data)
+        while True:
+            op4InMin = input("Enter minimum amount (Enter 'x' to exit ) > ")
+            op4InMax = input("Enter maximum amount (Enter 'x' to exit ) > ")
+            if op4InMin == 'x' or op4InMax == 'x':
+                view_expenses(extract)
+                break
+            try:
+                op4InMin = int(op4InMin)
+                if op4InMin < 0:
+                    print('Invalid amount, please enter minimum amount greater than 0')
+                    continue
+            except ValueError:
+                print('Invalid amount, please enter minimum amount greater than 0')
+                continue
+            try:
+                op4InMax = int(op4InMax)
+                if op4InMax < 0:
+                    print('Invalid amount, please enter maximum amount greater than 0')
+                    continue
+            except ValueError:
+                print('Invalid amount, please enter maximum amount greater than 0')
+                continue
+
+            if (op4InMin > op4InMax):
+                print('Invalid amount, please enter minimum amount greater than maximum amount')
+                continue
+
+            filtered_data = filter_expenses(df=df, min_amount=op4InMin, max_amount=op4InMax)
+            if extract is False:
+                print(filtered_data)
+            else:
+                save_to_csv(filtered_data,'based on the filter by amount')
+            break
+
     elif option == 'x':
         main_page()
-    view_expenses()
+
+    else:
+        print('\n---- Invalid Option ----\n')
+    view_expenses(extract)
+
+
+def save_to_csv(data, text):
+    file_name = 'expense_extract.csv'
+    data.to_csv(file_name, index=False)
+    print(f"{file_name} file created {text}")
 
 def main_page():
   exitFlag = False
@@ -92,15 +179,20 @@ def main_page():
         print('Personal Expense Tracker')
         print('-------------------------')
         print('1) View Expenses')
+        print('2) Extract Expenses')
         print('x) Exit')
         print('-------------------------')
         option = input('Choose an option > ')
         if option == '1':
-            print(option)
-            view_expenses()
+            view_expenses(extract = False)
+        elif option == '2':
+            view_expenses(extract = True)
         elif option == 'x':
             exitFlag = True
             print('Exiting...')
+        else:
+            print('\n---- Invalid Option ----\n')
+
     exit()
   except Exception:
     print('Program error')
